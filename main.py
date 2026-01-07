5be6d1c8bf5dc5b7b514f025a7d06c3df45e8fba
Sebastian <sebastian.dorobantul@gmail.com>
2026-01-07 12:02:39 +0100

    one more things

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QApplication, QFormLayout, QLabel, QMainWindow, QPushButton, QVBoxLayout, QWidget
 
 
 class MainWindow(QMainWindow):
-    def __init__(self):
+
+    def __init__(self) -> None:
         super().__init__()
         self.setWindowTitle("Mod6 UX prototype")
-        self.setGeometry(100, 100, 400, 300)
+        self.setGeometry(5000, 5000, 700,1000)
         
         # Create central widget and layout
         central_widget = QWidget()
+
         self.setCentralWidget(central_widget)
         layout = QVBoxLayout(central_widget)
-        
+        stats = QFormLayout()
+
+        layout.addLayout(stats)
+        stats.addRow("Exercise State:", QLabel("Ur sleepy"))
+
         # Create button
         button = QPushButton("I WANT AI")
         button.clicked.connect(self.on_button_clicked)
-        layout.addWidget(button)
+        stats.addRow("Do you want AI?:",button)
     
     def on_button_clicked(self):
-        print("Button clicked!") #TODO  STEFANO CONNECTS THIS TO HIS SHIT
+        print("HE WANTS AI!!!!!!!!!!!!!!!!!!!!!!!!!!") #TODO  STEFANO CONNECTS THIS TO HIS SHIT
 
 
 def main():
